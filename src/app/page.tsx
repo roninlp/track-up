172bc42d1cf5585cf42d5c066543d2462ea78187
ronin <ronin.lp@gmail.com>
2024-09-30 21:23:15 +0330

    format and type fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,14 @@ import Link from "next/link";
 
 import { HydrateClient } from "@/trpc/server";
 import { getSession } from "@/server/auth/lucia";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Calendar } from "@/components/ui/calendar";
 
 export default async function Home() {
   const { user } = await getSession();
@@ -13,28 +21,37 @@ export default async function Home() {
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
+            <Card>
+              <CardHeader>
+                <CardTitle>First Steps</CardTitle>
+                <CardDescription>
+                  Just the basics - Everything you need to know to set up your
+                  database and authentication.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <div className="text-lg">
+                  Just the basics - Everything you need to know to set up your
+                  database and authentication.
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>Documentation</CardTitle>
+                <CardDescription>
+                  Learn more about Create T3 App, the libraries it uses, and how
+                  to deploy it.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <div className="text-lg">
+                  Learn more about Create T3 App, the libraries it uses, and how
+                  to deploy it.
+                </div>
+                <Calendar mode="single" className="rounded-md border" />
+              </CardContent>
+            </Card>
           </div>
           <div className="flex flex-col items-center gap-2">
             hello {user?.email}
